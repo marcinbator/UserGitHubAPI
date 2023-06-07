8fcdcd12e006f724b574d33fb30b29c4319c30a2
Marcinek <marcinbator.ofc@gmail.com>
2023-06-07 17:36:50 +0200

    fix to error handling

--- src/main/java/com/example/usergithubapi/RepoController.java
+++ src/main/java/com/example/usergithubapi/RepoController.java
@@ -2,7 +2,6 @@ package com.example.usergithubapi;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,25 +15,25 @@ import java.util.Map;
 @RestController
 public class RepoController {
 
+
     @GetMapping("/api/{username}")
     public ResponseEntity<?> getRepositoriesJson(@RequestHeader(HttpHeaders.ACCEPT) String acceptHeader, @PathVariable String username) throws IOException {
-//        if(acceptHeader.equals(MediaType.APPLICATION_JSON_VALUE)){
-//            if(RepoService.ifUserExists(username)){
-//                return ResponseEntity.ok(RepoService.getRepos(username));
-//            }
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(createErrorResponse(HttpStatus.NOT_FOUND.value(), "User not found"));
-//        }
-//        if(acceptHeader.equals(MediaType.APPLICATION_XML_VALUE)){
-//            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(createErrorResponse(HttpStatus.NOT_ACCEPTABLE.value(), "XML not supported."));
-//        }
-//        return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body(createErrorResponse(HttpStatus.REQUEST_TIMEOUT.value(), "Connection failed - GitHub API rate exceeded."));
-        return ResponseEntity.ok(RepoService.getRepos(username));
+        if (acceptHeader.equals("application/json")){
+            return ResponseEntity.ok(RepoService.getRepos(username));
+        }
+        if (acceptHeader.equals("application/xml")) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(createErrorResponse(HttpStatus.NOT_ACCEPTABLE.value(), "Unsupported media type: application/xml."));
+        }
+        if (!RepoService.ifUserExists(username)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(createErrorResponse(HttpStatus.NOT_FOUND.value(), "User not found."));
+        }
+        return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body(createErrorResponse(HttpStatus.REQUEST_TIMEOUT.value(), "Request timeout."));
     }
 
-    private Map<String, Object> createErrorResponse(int status, String message) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("Message", message);
-        errorResponse.put("status", status);
+    private Map<String, String> createErrorResponse(int responseCode, String errorMessage) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("status", String.valueOf(responseCode));
+        errorResponse.put("Message", errorMessage);
         return errorResponse;
     }
 }
--- src/main/java/com/example/usergithubapi/RepoService.java
+++ src/main/java/com/example/usergithubapi/RepoService.java
@@ -3,30 +3,23 @@ package com.example.usergithubapi;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
-//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
 
 public class RepoService {
     public static ArrayList<RepoToShow> getRepos(String username) throws IOException {
         ObjectMapper objectMapper=new ObjectMapper();
         ArrayList<Repo> repos=objectMapper.readValue(new URL("https://api.github.com/users/"+username+"/repos"), new TypeReference<>() {});
-        ArrayList<Repo> reposToRemove=new ArrayList<>();
+        List<Repo>repos2=repos.stream().filter(repo-> !repo.isFork()).toList();
         ArrayList<RepoToShow> reposToShow=new ArrayList<>();
-        for(Repo repo:repos){
-            if(repo.fork){
-                reposToRemove.add(repo);
-            }
-            ArrayList<Branch> branches=objectMapper.readValue(new URL("https://api.github.com/repos/marcinbator/" + repo.name.toLowerCase() + "/branches"), new TypeReference<>() {});
+        for(Repo repo:repos2){
+            ArrayList<Branch> branches=objectMapper.readValue(new URL("https://api.github.com/repos/marcinbator/" + repo.getName().toLowerCase() + "/branches"), new TypeReference<>() {});
             repo.setBranches(branches);
-        }
-        repos.removeAll(reposToRemove);
-        for(Repo repo:repos){
-            reposToShow.add(new RepoToShow(repo.id, repo.name, repo.owner, repo.branches));
+            reposToShow.add(new RepoToShow(repo.getId(), repo.getName(), repo.getOwner(), repo.getBranches()));
         }
         return reposToShow;
     }
@@ -34,9 +27,12 @@ public class RepoService {
     public static boolean ifUserExists(String username){
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://api.github.com/users/"+username+"/repos";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        String responseBody = response.getBody();
-        assert responseBody != null;
-        return !responseBody.equals("404");
+        try{
+            restTemplate.getForEntity(url, String.class);
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
     }
 }
